c5173bf242a10edce43f851b2ecf12aba796d585
ShaiMesisterano <shai@mcterano.com>
2019-10-27 13:02:32 +0200

    modify solution - log result

--- exercises/01/max.js
+++ exercises/01/max.js
@@ -1,12 +1,13 @@
-Array.prototype.max =  function (){
+Array.prototype.max = function () {
     let max = undefined;
-     for(let i=0;i<this.length;i++){
-         if(!max || max<this[i])
+    for (let i = 0; i < this.length; i++) {
+        if (!max || max < this[i])
             max = this[i];
-         }
-         return max;
-    };
+    }
+    return max;
+};
 
 
 var arr = [1, 22, 50, 7];
 var maximum = arr.max();
+console.log("maximum", maximum);
--- exercises/01/min.js
+++ exercises/01/min.js
@@ -1,12 +1,13 @@
-Array.prototype.min =  function (){
+Array.prototype.min = function () {
     let min = undefined;
-     for(let i=0;i<this.length;i++){
-         if(!min || min>this[i])
+    for (let i = 0; i < this.length; i++) {
+        if (!min || min > this[i])
             min = this[i];
-         }
-         return min;
-    };
+    }
+    return min;
+};
 
 
 var arr = [1, 22, 50, 7];
 var minimum = arr.min();
+console.log("minimum", minimum);
